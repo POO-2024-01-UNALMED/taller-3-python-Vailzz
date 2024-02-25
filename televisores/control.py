0460e896232f3b0fbd4141ff236d83a5cdaed43b
Juan Villa <jujotovil@gmail.com>
2024-02-24 23:01:32 -0500

    Second commit

--- televisores/control.py
+++ televisores/control.py
@@ -1,14 +1,31 @@
-from televisores import TV
-
 class Control:
-    def __init__(self):
-        self.tv = TV
-
-
-    def enlazar(self, televisor):
-        self.tv = televisor
-
-    def getTv(self):
-        return self.tv
-    def setTv(self, televisor):
-        self.tv = televisor
+    def __init__(self, tv):
+        self.tv = tv
+    
+    def enlazar(self, tv):
+        self.tv = tv
+        self.tv.setControl(self)
+    
+    def turnOn(self):
+        self.tv.turnOn()
+    
+    def turnOff(self):
+        self.tv.turnOff()
+    
+    def canalUp(self):
+        self.tv.setCanal(self.tv.getCanal() + 1)
+    
+    def canalDown(self):
+        self.tv.setCanal(self.tv.getCanal() - 1)
+    
+    def volumenUp(self):
+        self.tv.setVolumen(self.tv.getVolumen() + 1)
+    
+    def volumenDown(self):
+        self.tv.setVolumen(self.tv.getVolumen() - 1)
+    
+    def setCanal(self, canal):
+        self.tv.setCanal(canal)
+    
+    def setVolumen(self, volumen):
+        self.tv.setVolumen(volumen)
--- televisores/tv.py
+++ televisores/tv.py
@@ -1,5 +1,9 @@
 from televisores import Control
+
 class TV:
+
+    numTv = 0
+
     def __init__(self, marca, estado):
         self.marca = marca
         self.canal = 1
@@ -7,37 +11,46 @@ class TV:
         self.estado = estado
         self.volumen = 1
         self.control = Control
-        self.numTv = 0
-
+    #Getter and Setter de Marca
     def getMarca(self):
         return self.marca
     def setMarca(self, marca):
         self.marca = marca
 
+#Getter and Setter de Canal
     def getCanal(self):
         return self.canal
     def setCanal(self, canal):
         self.canal = canal
 
+#Getter and Setter de Precio
     def getPrecio(self):
         return self.precio
     def setPrecio(self, precio):
         self.precio = precio
+
+#Getter and Setter de Volumen
 
     def getVolume(self):
         return self.volumen
     def setVolume(self, volumen):
         self.volumen = volumen
 
+
+#Getter and Setter de Control
     def getControl(self):
         return self.control
     def setControl(self, control):
         self.control = control
 
+#Getter and Setter de numTv
     def getNumTV(self):
         return self.numTv
+    
     def setNumTV(self, numTv):
         self.numTv = numTv
+
+
 
     def turnOn(self):
         self.estado = True

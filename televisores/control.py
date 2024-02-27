9d72c130915ed2ca0aa347ac156d8d1ef1be1170
Juan Tobon <jujotovil@gmail.com>
2024-02-26 19:04:51 -0500

    changes at TV and Control

--- televisores/control.py
+++ televisores/control.py
@@ -1,32 +1,29 @@
-from televisores.tv import TV
 class Control:
-    def __init__(self, tv):
-        self._tv = tv
-    
-    def enlazar(self, tv):
-        self.tv = tv
-        tv.setControl(self)
-    
+    _tv = None
+
     def turnOn(self):
         self._tv.turnOn()
-    
     def turnOff(self):
         self._tv.turnOff()
-    
     def canalUp(self):
-        self._tv.setCanal(self.tv.getCanal() + 1)
-    
+        self._tv.canalUp()
     def canalDown(self):
-        self._tv.setCanal(self.tv.getCanal() - 1)
-    
+        self._tv.canalDown()
     def volumenUp(self):
-        self._tv.setVolumen(self.tv.getVolumen() + 1)
-    
+        self._tv.volumenUp()
     def volumenDown(self):
-        self._tv.setVolumen(self.tv.getVolumen() - 1)
+        self._tv.volumenDown()
+    def setCanal(self,num):
+        self._tv.setCanal(num)
+
+    def setVolumen(self,num):
+        self._tv.setVolumen(num)
     
-    def setCanal(self, canal):
-        self._tv.setCanal(canal)
-    
-    def setVolumen(self, volumen):
-        self._tv.setVolumen(volumen)
+    def enlazar(self,tv):
+        self._tv = tv
+        tv.setControl(self)
+
+    def setTv(self,tv):
+        self.enlazar(tv)
+    def getTv(self):
+        return self._tv
--- televisores/tv.py
+++ televisores/tv.py
@@ -1,4 +1,3 @@
-from televisores import Control
 class TV:
 
     _numTv = 0
@@ -9,72 +8,72 @@ class TV:
         self._precio = 500
         self._estado = estado
         self._volumen = 1
-        self._control = Control
+        self._control = None
 
         TV._tv+=1
+        
     #Getter and Setter de Marca
     def getMarca(self):
-        return self.marca
+        return self._marca
     def setMarca(self, marca):
         self._marca = marca
 
 #Getter and Setter de Canal
     def getCanal(self):
         return self.canal
-    def setCanal(self, canal):
-        self.canal = canal
+    def setCanal(self, i):
+        if (self._estado == True) and (i <=120 ) and ( i>=1 ):
+            self._canal = i
 
 #Getter and Setter de Precio
     def getPrecio(self):
-        return self.precio
+        return self._precio
     def setPrecio(self, precio):
-        self.precio = precio
+        self._precio = precio
 
 #Getter and Setter de Volumen
 
     def getVolume(self):
-        return self.volumen
+        return self._volumen
     def setVolume(self, volumen):
-        self.volumen = volumen
+        if self._estado == True and volumen>=0 and volumen <= 7:
+            self._volumen = volumen
 
 
 #Getter and Setter de Control
     def getControl(self):
-        return self.control
+        return self._control
     def setControl(self, control):
-        self.control = control
+        self._control = control
 
 #Getter and Setter de numTv
     @classmethod
     def getNumTV(self):
-        return T.numTv
+        return TV._numTv
     @classmethod
-    def setNumTV(self, numTv):
-        self.numTv = numTv
+    def setNumTV(self, num):
+        TV._numTv = num
 
 
 
     def turnOn(self):
-        self.estado = True
+        self._estado = True
     def turnOff(self):
-        self.estado = False
+        self._estado = False
 
     def getEstado(self):
-        return self.estado
+        return self._estado
     
     def canalUp(self):
-        if self.estado == True and 1 >= self.canal <= 120:
-            self.canal += 1
-
+        if (self._estado == True)and(self._canal<120):
+            self._canal +=1
     def canalDown(self):
-        if self.estado == True and 1 >= self.canal <= 120:
-            self.canal -= 1
-    
+        if (self._estado == True)and(self._canal>1):
+            self._canal -=1
     def volumenUp(self):
-        if self.estado == True and 1 >= self.volumen <= 7:
-            self.volumen += 1
-    
+        if (self._estado == True)and(self._volumen<7):
+            self._volumen +=1
     def volumenDown(self):
-        if self.estado == True and 1 >= self.volumen <= 7:
-            self.volumen -= 1
+        if (self._estado == True)and(self._volumen>0):
+            self._volumen -=1
     
